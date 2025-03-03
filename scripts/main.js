9c88c3dbf762cf37ebe1773539b9e767ad503ebe
SoteRN <sriveran@irsl.edu.mx>
2025-03-03 08:38:46 -0600

    Copiando archivos

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,39 @@
-function Dean(){
-    var image = document.getElementById ("deanFromHell"); 
+function dean(){
+    var image=document.getElementById("deanFromHell"); 
     console.log(image.src);
-    if (image.src.includes("musicDime.png")) {
-        image.src = "Images/musicDime1. jpg";
+    if (image.src.match("musicDime1")) {
+        image.src="images/musicDime.png";
     } else {
-        image.src = "Images/musicDime1.png";
+        image.src="images/musicDime1.png";
+    }
+}
+
+function mueble(){
+    var image=document.getElementById("furniture"); 
+    console.log(image.src);
+    if (image.src.match("Mueble_a")) {
+        image.src="images/Mueble.jpg";
+    } else {
+        image.src="images/Mueble_a.jpg";
+    }
+}
+
+function iphone(){
+    var image=document.getElementById("iphone16"); 
+    console.log(image.src);
+    if (image.src.match("Electronic_a")) {
+        image.src="images/electronic.png";
+    } else {
+        image.src="images/Electronic_a.png";
+    }
+}
+
+function hoodie(){
+    var image=document.getElementById("trenHoodie"); 
+    console.log(image.src);
+    if (image.src.match("clothe_a")) {
+        image.src="images/clothe.png";
+    } else {
+        image.src="images/clothe_a.jpg";
     }
 }
